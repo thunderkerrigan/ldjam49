e188f5d3c242bf3cafe3988278d917fbd7ec6bc6
Briqn <pitet.brian@gmail.com>
2021-10-02 12:08:09 +0200

    Game A TableTampon creation

---
+++ Scripts/Countdown.gd
@@ -0,0 +1,25 @@
+extends Container
+
+export (int) var seconds = 0
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	$Label.set_text(str(seconds))
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_CountdownTimer_timeout():
+	seconds -= 1
+	
