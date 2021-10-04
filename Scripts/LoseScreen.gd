de6e760d059b2f4c3feaf578c8800533fdc718fa
Briqn <pitet.brian@gmail.com>
2021-10-04 16:06:11 +0200

    Screen Animation

--- Scripts/LoseScreen.gd
+++ Scripts/LoseScreen.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
-onready var timer = get_node("Timer")
+signal ended
 
+var SoundClip = preload("res://Scenes/SoundEffect.tscn")
 
-func _on_Timer_timeout():
+var clip1 = preload("res://Assets/Sounds/Start/game-start-6104.mp3")
+var clip2 = preload("res://Assets/Sounds/Start/level-winwav-6416.mp3")
+var clip3 = preload("res://Assets/Sounds/Start/boingwav-6222.mp3")
+var clip4 = preload("res://Assets/Sounds/Start/chime-sound-7143.mp3")
+var clip5 = preload("res://Assets/Sounds/Start/success-1-6297.mp3")
+var clips = [clip1, clip2, clip3, clip4, clip5]
+
+func start():
+	randomize()
+	var clip = clips[randi() % clips.size()]
+	var audio = SoundClip.instance()
+	add_child(audio)
+	if clip.get_length() > 1.5:
+		audio.pitch_scale = clip.get_length()/1.8	
+	audio.start(clip)
+	yield(get_tree().create_timer(1.5), "timeout")
 	print("Fin de pause")
-	get_tree().paused = false
+	emit_signal("ended")
 	hide()
 
-
-func _on_Clickable_clicked():
-	show()
-	timer.set_wait_time(3)
-	timer.start()
-	print("start pause")
-	get_tree().paused = true
-	
-	
-	
+func _ready():
+	start()
